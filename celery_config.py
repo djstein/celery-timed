3aa74c0f4f2f2dfeca74dff5d7af199513ec7626
Dylan Stein <djstein@ncsu.edu>
2017-02-14 20:12:13 -0500

    change schedule name

--- celery_config.py
+++ celery_config.py
@@ -8,13 +8,13 @@ app = Celery(
 )
 
 # Time currently shown to print the test function every 10 seconds
-time = 10.0
-
+# time = 10.0
+time = crontab(day_of_week='tuesday')
 # This can be changed to work at any cron time, for example, midnight:
 # time = crontab(hour=0, minute=0)
 
 app.conf.beat_schedule = {
-    'add-every-monday-morning': {
+    'function-at-time': {
         'task': 'tasks.test_function',
         'schedule': time,
         'args': [str(time)],
